d009c1d3fc7a00cc6a5034c9628ee4d3a96ee72e
Alex Miller <alex.k.miller@gmail.com>
2018-12-12 08:55:50 -0500

    LEFT join entity names

--- src/db.ts
+++ src/db.ts
@@ -23,27 +23,37 @@ export class DB {
     }
     public single_table(column_names, indicator, entities?, start_year = "2000", end_year = "2015", limit = "1000000", offset = "0"): Promise<any>{
         let entity_name = "di_id"
+        let entity_table = "di_entity"
         if(column_names.includes("di_id")){
           entity_name = "di_id"
+          entity_table = "di_entity"
         }else if(column_names.includes("to_di_id")){
           entity_name = "to_di_id"
+          entity_table = "di_entity"
         }else if(column_names.includes("district_id")){
           entity_name = "district_id"
+          if(indicator.includes("kenya")){
+            entity_table = "ref_kenya_district"
+          }else if(indicator.includes("uganda")){
+            entity_table = "ref_uganda_district"
+          }else{
+            return this.db.any("SELECT * FROM $1~ LIMIT $2 OFFSET $3;", [indicator, limit, offset])
+          }
         }else{
           return this.db.any("SELECT * FROM $1~ LIMIT $2 OFFSET $3;", [indicator, limit, offset])
         }
         if(column_names.includes("year")){
           if(entities != null){
             let entities_arr = entities.split(",")
-            return this.db.any("SELECT * FROM $1~ WHERE year >= $2 AND year <= $3 AND $4~ IN ($5:csv) ORDER BY $4~ ASC, year ASC LIMIT $6 OFFSET $7;", [indicator, start_year, end_year, entity_name, entities_arr, limit, offset])
+            return this.db.any("SELECT $1~.*, $8~.name FROM $1~ LEFT JOIN $8~ ON $1~.$4~ = $8~.id WHERE year >= $2 AND year <= $3 AND $4~ IN ($5:csv) ORDER BY $4~ ASC, year ASC LIMIT $6 OFFSET $7;", [indicator, start_year, end_year, entity_name, entities_arr, limit, offset, entity_table])
           }
-          return this.db.any("SELECT * FROM $1~ WHERE year >= $2 AND year <= $3 ORDER BY $4~ ASC, year ASC LIMIT $5 OFFSET $6;", [indicator, start_year, end_year, entity_name, limit, offset])
+          return this.db.any("SELECT $1~.*, $7~.name FROM $1~ LEFT JOIN $7~ ON $1~.$4~ = $7~.id WHERE year >= $2 AND year <= $3 ORDER BY $4~ ASC, year ASC LIMIT $5 OFFSET $6;", [indicator, start_year, end_year, entity_name, limit, offset, entity_table])
         }else{
           if(entities != null){
             let entities_arr = entities.split(",")
-            return this.db.any("SELECT * FROM $1~ WHERE $2 IN ($3:csv) ORDER BY $2~ ASC LIMIT $4 OFFSET $5;", [indicator, entity_name, entities_arr, limit, offset])
+            return this.db.any("SELECT $1~.*, $6~.name FROM $1~ LEFT JOIN $6~ ON $1~.$2~ = $6~.id WHERE $2 IN ($3:csv) ORDER BY $2~ ASC LIMIT $4 OFFSET $5;", [indicator, entity_name, entities_arr, limit, offset, entity_table])
           }
-          return this.db.any("SELECT * FROM $1~ WHERE ORDER BY $2~ ASC LIMIT $3 OFFSET $4;", [indicator, entity_name, limit, offset])
+          return this.db.any("SELECT $1~.*, $5~.name FROM $1~ LEFT JOIN $5~ ON $1~.$2~ = $5~.id WHERE ORDER BY $2~ ASC LIMIT $3 OFFSET $4;", [indicator, entity_name, limit, offset, entity_table])
         }
 
     }
