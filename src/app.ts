5eceee61c5a60247b5c79340cc311ba907ceb4a2
Edwin P. Magezi <edwinm_p@yahoo.com>
2019-01-16 13:02:07 +0300

    [Refactor] add tslint & strict tsconfig rules

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,29 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as Ddos from 'ddos';
-import { Index } from "./routes/index";
+import { Index } from './routes/index';
 
 class App {
+  public app: express.Application;
+  public indexRoutes: Index = new Index();
 
-    public app: express.Application;
-    public indexRoutes: Index = new Index();
+  constructor() {
+    const ddos = new Ddos();
+    this.app = express();
+    this.app.use(express.static('public'));
+    this.app.use(ddos.express);
+    this.config();
+    this.indexRoutes.routes(this.app);
+  }
 
-    constructor() {
-        const ddos = new Ddos();
-        this.app = express();
-        this.app.use(express.static('public'))
-        this.app.use(ddos.express)
-        this.config();
-        this.indexRoutes.routes(this.app);
-    }
-
-    private config(): void {
-        this.app.use(bodyParser.json());
-        this.app.use(bodyParser.urlencoded({
-            extended: false
-        }));
-    }
-
+  private config(): void {
+    this.app.use(bodyParser.json());
+    this.app.use(
+      bodyParser.urlencoded({
+        extended: false
+      })
+    );
+  }
 }
 
 export default new App().app;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,34 +1,39 @@
-import {Request, Response} from "express";
-import {DB} from "../db"
+import { Application, Request, Response } from 'express';
+import { DB } from '../db';
 
+type ResponseFormat = 'json' | 'csv' | 'xml';
 export class Index {
 
-    public dbConn: DB = new DB();
+  dbConn: DB = new DB();
 
-    public send_data(res, data, format?, indicator?): void {
-      let file_extension = format?format:"json"
-      let file_name = indicator?indicator:"download"
-      res.setHeader('Content-disposition', 'inline; filename='+file_name+'.'+file_extension);
-      res.setHeader('Content-Type', 'application/'+file_extension)
-      res.status(200).send(this.dbConn.format_data(data, format));
-    }
+  send_data(res: Response, data: any, format?: ResponseFormat, indicator?: string): void {
+    const file_extension = format ? format : 'json';
+    const file_name = indicator ? indicator : 'download';
+    res.setHeader('Content-disposition', 'inline; filename=' + file_name + '.' + file_extension);
+    res.setHeader('Content-Type', 'application/' + file_extension);
+    res.status(200).send(this.dbConn.format_data(data, format));
+  }
 
-    public send_error(res, error, format?): void {
-      let file_extension = format?format:"json"
-      res.setHeader('Content-disposition', 'inline; filename=error.'+file_extension);
-      res.setHeader('Content-Type', 'application/'+file_extension)
-      res.status(200).send(this.dbConn.format_error(error, format));
-    }
-    public routes(app): void {
-        app.route('/')
-        .get((req: Request, res: Response) => {
-            res.status(200).send(`
+  send_error(res: Response, error: { code: string }, format?: ResponseFormat): void {
+    const file_extension = format ? format : 'json';
+    res.setHeader('Content-disposition', 'inline; filename=error.' + file_extension);
+    res.setHeader('Content-Type', 'application/' + file_extension);
+    res.status(200).send(this.dbConn.format_error(error, format));
+  }
+
+  routes(app: Application): void {
+    // tslint:disable max-line-length
+    app.route('/')
+      .get((_req: Request, res: Response) => {
+        res.status(200).send(`
             <html>
               <h1>DDW API</h1>
               <h2>Endpoints</h2>
               <ul>
                 <li><a href="/all_tables">/all_tables</a></li>
-                <li><a href="/meta_data">/meta_data</a> (just serves di_concept_in_dh, not too useful at the moment)</li>
+                <li>
+                  <a href="/meta_data">/meta_data</a> (just serves di_concept_in_dh, not too useful at the moment)
+                </li>
                 <li>/single_table</li>
                 <li>/multi_table</li>
               </ul>
@@ -43,7 +48,11 @@ export class Index {
                 <li><b>format</b>: ?format=xml (available options are xml, json, or csv)</li>
               </ul>
               <h3>Example query</h3>
-              <p><a href="/single_table?indicator=population_total&entities=UG,KE&start_year=2000&end_year=2000&limit=2&offset=0&format=xml">/single_table?indicator=population_total&entities=UG,KE&start_year=2000&end_year=2000&limit=2&offset=0&format=xml</a></p>
+              <p>
+                <a href="/single_table?indicator=population_total&entities=UG,KE&start_year=2000&end_year=2000&limit=2&offset=0&format=xml">
+                  /single_table?indicator=population_total&entities=UG,KE&start_year=2000&end_year=2000&limit=2&offset=0&format=xml
+                </a>
+              </p>
 
               <h2>Parameters for /multi_table</h2>
               <ul>
@@ -56,92 +65,102 @@ export class Index {
                 <li><b>format</b>: ?format=xml (available options are xml, json, or csv)</li>
               </ul>
               <h3>Example query</h3>
-              <p><a href="/multi_table?indicators=population_total,govt_revenue_pc_gdp&entities=UG&start_year=2015&end_year=2015&format=xml">/multi_table?indicators=population_total,govt_revenue_pc_gdp&entities=UG&start_year=2015&end_year=2015&format=xml</a></p>
+              <p>
+                <a href="/multi_table?indicators=population_total,govt_revenue_pc_gdp&entities=UG&start_year=2015&end_year=2015&format=xml">
+                  /multi_table?indicators=population_total,govt_revenue_pc_gdp&entities=UG&start_year=2015&end_year=2015&format=xml
+                </a>
+              </p>
 
             </html>
             `);
-        })
-        app.route('/single_table')
-        .get((req: Request, res: Response) => {
-            if(this.dbConn.forbidden_tables.indexOf(req.query.indicator) > -1){
-              this.send_error(res, {"code":"403"}, req.query.format)
-            }else{
-              this.dbConn.column_names(req.query.indicator)
-                .then((c_names) => {
-                  let table_keys = Object.keys(c_names[0]);
-                  this.dbConn.single_table(
-                    table_keys,
-                    req.query.indicator,
-                    req.query.entities,
-                    req.query.start_year,
-                    req.query.end_year,
-                    req.query.limit,
-                    req.query.offset
-                  )
-                  .then((data) => {
-                    this.send_data(res, data, req.query.format, req.query.indicator)
-                  }).catch((error) => {
-                    this.send_error(res, error, req.query.format)
-                  });
-                }).catch((error) => {
-                  this.send_error(res, error, req.query.format)
-                });
-            };
-        });
-        app.route('/multi_table')
-        .get((req: Request, res: Response) => {
-          let indicators = req.query.indicators.split(",");
-          let valid_indicators = indicators.filter((item)=>{
-            return this.dbConn.forbidden_tables.indexOf(item) === -1;
-          });
-            this.dbConn.column_names(valid_indicators[0])
-              .then((c_names) => {
-                let table_keys = Object.keys(c_names[0]);
-                this.dbConn.multi_table(
-                  table_keys,
-                  valid_indicators,
-                  req.query.entities,
-                  req.query.start_year,
-                  req.query.end_year,
-                  req.query.limit,
-                  req.query.offset
-                ).then((data_arr) => {
-                  let master_data = [];
-                  data_arr.forEach((item, index)=>{
-                    let matching_indicator = valid_indicators[index];
-                    let data_with_ind = item.map((el)=>{
-                      let o = Object.assign({}, el);
-                      o.indicator = matching_indicator;
-                      return o;
-                    });
-                    master_data = master_data.concat(data_with_ind);
-                  });
+      });
+    // tslint:enable max-line-length
 
-                  this.send_data(res, master_data, req.query.format, req.query.indicator)
+    app.route('/single_table')
+      .get((req: Request, res: Response) => {
+        if (this.dbConn.forbidden_tables.indexOf(req.query.indicator) > -1) {
+          this.send_error(res, { code: '403' }, req.query.format);
+        } else {
+          this.dbConn.column_names(req.query.indicator)
+            .then((c_names) => {
+              const table_keys = Object.keys(c_names[0]);
+              this.dbConn.single_table(
+                table_keys,
+                req.query.indicator,
+                req.query.entities,
+                req.query.start_year,
+                req.query.end_year,
+                req.query.limit,
+                req.query.offset
+              )
+                .then((data) => {
+                  this.send_data(res, data, req.query.format, req.query.indicator);
                 }).catch((error) => {
-                  this.send_error(res, error, req.query.format)
+                  this.send_error(res, error, req.query.format);
                 });
-              }).catch((error) => {
-                this.send_error(res, error, req.query.format)
-              });
+            }).catch((error) => {
+              this.send_error(res, error, req.query.format);
+            });
+        }
+      });
+
+    app.route('/multi_table')
+      .get((req: Request, res: Response) => {
+        const indicators: string[] = req.query.indicators.split(',');
+        const valid_indicators = indicators.filter((item) => {
+          return this.dbConn.forbidden_tables.indexOf(item) === -1;
         });
-        app.route('/all_tables')
-        .get((req: Request, res: Response) => {
-            this.dbConn.all_tables()
-              .then((data) => {
-                this.send_data(res, data, req.query.format)
-              }).catch((error) => {
-                this.send_error(res, error, req.query.format)
+        this.dbConn.column_names(valid_indicators[0])
+          .then((c_names) => {
+            const table_keys = Object.keys(c_names[0]);
+            this.dbConn.multi_table(
+              table_keys,
+              valid_indicators,
+              req.query.entities,
+              req.query.start_year,
+              req.query.end_year,
+              req.query.limit,
+              req.query.offset
+            ).then((data_arr: any[]) => {
+              let master_data: any[] = [];
+              data_arr.forEach((item, index) => {
+                const matching_indicator = valid_indicators[index];
+                const data_with_ind = item.map((el: any) => {
+                  const o = { ...el };
+                  o.indicator = matching_indicator;
+
+                  return o;
+                });
+                master_data = master_data.concat(data_with_ind);
               });
-        });
-        app.route('/meta_data')
-        .get((req: Request, res: Response) => {
-            this.dbConn.meta_data()
-              .then((data) => {
-                this.send_data(res, data, req.query.format)
-              }).catch((error) => {
-                this.send_error(res, error, req.query.format)
-              });
-        });
-    };
-};
+
+              this.send_data(res, master_data, req.query.format, req.query.indicator);
+            }).catch((error) => {
+              this.send_error(res, error, req.query.format);
+            });
+          }).catch((error) => {
+            this.send_error(res, error, req.query.format);
+          });
+      });
+
+    app.route('/all_tables')
+      .get((req: Request, res: Response) => {
+        this.dbConn.all_tables()
+          .then((data) => {
+            this.send_data(res, data, req.query.format);
+          }).catch((error) => {
+            this.send_error(res, error, req.query.format);
+          });
+      });
+
+    app.route('/meta_data')
+      .get((req: Request, res: Response) => {
+        this.dbConn.meta_data()
+          .then((data) => {
+            this.send_data(res, data, req.query.format);
+          }).catch((error) => {
+            this.send_error(res, error, req.query.format);
+          });
+      });
+  }
+}
