90db1106b8a0595d20fb9a941a1afa9f4586101f
Edwin P. Magezi <edwinm_p@yahoo.com>
2019-01-16 15:07:21 +0300

    [Refactor] dev dependencies and main files

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,23 @@
 import * as express from 'express';
-import * as bodyParser from 'body-parser';
+import { json, urlencoded } from 'body-parser';
 import * as Ddos from 'ddos';
-import { Index } from './routes/index';
+import { Index } from './routes';
 
 class App {
-  public app: express.Application;
-  public indexRoutes: Index = new Index();
+  app: express.Application = express();
+  indexRoutes: Index = new Index();
 
   constructor() {
-    const ddos = new Ddos();
-    this.app = express();
-    this.app.use(express.static('public'));
-    this.app.use(ddos.express);
     this.config();
     this.indexRoutes.routes(this.app);
   }
 
   private config(): void {
-    this.app.use(bodyParser.json());
+    this.app.use(express.static('public'));
+    this.app.use(new Ddos().express);
+    this.app.use(json());
     this.app.use(
-      bodyParser.urlencoded({
+      urlencoded({
         extended: false
       })
     );
--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import app from './app';
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-    console.log('listening on port ' + PORT);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
