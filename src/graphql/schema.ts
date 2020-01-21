0bf74592dfc41565d98a2bab1deba5d9f99fc85f
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-01-21 14:42:48 +0300

    Add documentation for geocode field

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -2,6 +2,9 @@ import { gql } from 'apollo-server-express';
 
 export const typeDefs = gql`
   type Data {
+    """
+    Format can be [CountryCode].[DistrictCode].[CountyCode] e.g UG.d102 == Kampala
+    """
     geocode: String
     year: Int
     value: Int
