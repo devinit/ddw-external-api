b4ad1155eba12612a687875326f853695d062d01
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-02-24 09:59:54 +0300

    Fix db.ts formatting

--- src/db.ts
+++ src/db.ts
@@ -1,4 +1,3 @@
-// tslint:disable max-line-length
 import * as fs from 'fs';
 import { IDatabase, IMain } from 'pg-promise';
 import * as PGPromise from 'pg-promise'; // tslint:disable-line:no-duplicate-imports
@@ -30,18 +29,213 @@ const initOptions = {
     } else if ((dc as string).includes('uganda')) {
       return 'spotlight_on_uganda_2017';
     } else {
-      return [ 'spotlight_on_uganda_2017', 'spotlight_on_kenya_2017', 'data_series', 'reference', 'fact', 'dimension', 'donor_profile', 'recipient_profile', 'multilateral_profile', 'south_south_cooperation' ];
+      return [
+        'spotlight_on_uganda_2017',
+        'spotlight_on_kenya_2017',
+        'data_series',
+        'reference',
+        'fact',
+        'dimension',
+        'donor_profile',
+        'recipient_profile',
+        'multilateral_profile',
+        'south_south_cooperation'
+      ];
     }
   }
 };
 
 const tableMappings = {
-  'uganda_indicator_data': [ 'uganda_anc4_coverage', 'uganda_avg_house_size', 'uganda_child_marriage_female', 'uganda_child_marriage_male', 'uganda_child_marriage_total', 'uganda_dependency_ratio', 'uganda_deprivation_living', 'uganda_disability', 'uganda_disability_hearing', 'uganda_disability_remembering', 'uganda_disability_seeing', 'uganda_disability_walking', 'uganda_dpt3_coverage', 'uganda_energy_cooking_charcoal', 'uganda_energy_cooking_electricity', 'uganda_energy_cooking_firewood', 'uganda_energy_cooking_gas', 'uganda_energy_cooking_paraffin_stove', 'uganda_energy_lighting_candle', 'uganda_energy_lighting_electricity', 'uganda_energy_lighting_firewood', 'uganda_energy_lighting_gas', 'uganda_energy_lighting_paraffin_lantern', 'uganda_energy_lighting_paraffin_tadooba', 'uganda_health_posts', 'uganda_hmis', 'uganda_household_number', 'uganda_household_san_cov', 'uganda_household_water_coverage', 'uganda_ipt2_coverage', 'uganda_leaving_exam_perf_rate', 'uganda_life_expectancy', 'uganda_overall_health', 'uganda_pop_dens', 'uganda_pop_growthrate', 'uganda_pop_sex_ratio', 'uganda_poverty_headcount', 'uganda_primary_enrol', 'uganda_primary_pupil_classroom_ratio', 'uganda_primary_pupil_classroom_ratio_gov', 'uganda_primary_pupil_classroom_ratio_priv', 'uganda_primary_pupil_stance_ratio', 'uganda_primary_pupil_stance_ratio_gov', 'uganda_primary_pupil_stance_ratio_priv', 'uganda_primary_sit_write', 'uganda_primary_sit_write_gov', 'uganda_primary_stu_teach_ratio', 'uganda_primary_stu_teach_ratio_gov', 'uganda_primary_stu_teach_ratio_priv', 'uganda_remittance', 'uganda_rural_safe_water', 'uganda_rural_water_func', 'uganda_secondary_enrol', 'uganda_secondary_sit_write', 'uganda_secondary_sit_write_gov', 'uganda_secondary_stu_classroom_ratio', 'uganda_secondary_stu_classroom_ratio_gov', 'uganda_secondary_stu_classroom_ratio_priv', 'uganda_secondary_stu_stance_ratio', 'uganda_secondary_stu_stance_ratio_gov', 'uganda_secondary_stu_stance_ratio_priv', 'uganda_secondary_stu_teach_ratio', 'uganda_secondary_stu_teach_ratio_gov', 'uganda_tb_success', 'uganda_total_pop', 'uganda_urban_pop', 'uganda_wash_perf_score', 'uganda_water_equity_rural', 'uganda_water_source_comm_func', 'uganda_water_women_position' ],
-  'uganda_indicator_percent': [ 'uganda_agri_percent', 'uganda_donor_percent', 'uganda_educ_percent', 'uganda_health_percent', 'uganda_local_percent' ],
-  'uganda_indicator_ncu': [ 'uganda_central_resources', 'uganda_donor_educ_spend', 'uganda_donor_resources', 'uganda_health_funding', 'uganda_igf_resources', 'uganda_primary_educ_funding' ],
-  'kenya_indicator_data': [ 'kenya_births_attendance', 'kenya_births_attendance_5_years', 'kenya_births_notified', 'kenya_births_notified_5_years', 'kenya_child_mortality', 'kenya_children_breastfed', 'kenya_children_stunted', 'kenya_children_underweight', 'kenya_children_wasting', 'kenya_contraceptives', 'kenya_dependency_ratio', 'kenya_diarrhoea', 'kenya_disability', 'kenya_disability_cognition', 'kenya_disability_communication', 'kenya_disability_hearing', 'kenya_disability_mobility', 'kenya_disability_self_care', 'kenya_disability_visual', 'kenya_disability_wasting', 'kenya_secondary_ner_boys', 'kenya_albinism', 'kenya_avg_house_size', 'kenya_primary_av_size_gov', 'kenya_primary_av_size_priv', 'kenya_primary_ner_boys', 'kenya_primary_ner_girls', 'kenya_primary_stu_teach_ratio', 'kenya_primary_stu_teach_ratio_gov', 'kenya_primary_stu_teach_ratio_priv', 'kenya_safe_water', 'kenya_secondary_av_size_gov', 'kenya_secondary_av_size_priv', 'kenya_secondary_ner_girls', 'kenya_secondary_stu_teach_ratio', 'kenya_secondary_stu_teach_ratio_gov', 'kenya_secondary_stu_teach_ratio_priv', 'kenya_total_pop', 'kenya_urban_pop', 'kenya_ecde_ner_boys', 'kenya_ecde_ner_girls', 'kenya_energy_cooking_charcoal', 'kenya_energy_cooking_electricity', 'kenya_energy_cooking_firewood', 'kenya_energy_lighting_candle', 'kenya_energy_lighting_electricity', 'kenya_energy_lighting_gaslamp', 'enya_energy_lighting_paraffin', 'kenya_energy_lighting_solar', 'kenya_energy_lighting_wood', 'kenya_hdi', 'kenya_health_insurance_prop_pop', 'kenya_hhs_crop_production', 'kenya_hhs_irrigation', 'kenya_hhs_livestock_prod', 'kenya_hiv_prevalence', 'kenya_household_female_head', 'kenya_household_number', 'kenya_household_san_com', 'kenya_household_san_fac', 'kenya_household_water_com', 'kenya_malaria_drugs_under5', 'kenya_maternal_mortality', 'kenya_measles_immunisation', 'kenya_measles_vaccine', 'kenya_mosquito_nets', 'kenya_mosquito_nets_under5', 'kenya_per_cap_agric', 'kenya_per_cap_educ', 'kenya_per_cap_health', 'kenya_per_cap_wash', 'kenya_pop_birthrate', 'kenya_pop_deathrate', 'kenya_pop_dens', 'kenya_pop_disability_engage_econ', 'kenya_pop_sex_ratio', 'kenya_pop_working', 'kenya_pop_working_female', 'kenya_pop_working_male', 'kenya_poverty_food', 'kenya_poverty_gap', 'kenya_poverty_hardcore', 'kenya_poverty_headcount', 'kenya_poverty_meanpp_exp' ],
-  'kenya_indicator_percent': [ 'kenya_educ_percent', 'kenya_health_percent', 'kenya_local_percent', 'kenya_conditional_percent', 'kenya_agri_percent', 'kenya_water_per_of_revenue', 'kenya_donor_percent', 'kenya_equitable_percent', 'kenya_gov_spend_pp' ],
-  'kenya_indicator_ncu': [ 'kenya_donor_resources', 'kenya_health_funding', 'kenya_igf_resources', 'kenya_central_resources' ]
+  uganda_indicator_data: [
+    'uganda_anc4_coverage',
+    'uganda_avg_house_size',
+    'uganda_child_marriage_female',
+    'uganda_child_marriage_male',
+    'uganda_child_marriage_total',
+    'uganda_dependency_ratio',
+    'uganda_deprivation_living',
+    'uganda_disability',
+    'uganda_disability_hearing',
+    'uganda_disability_remembering',
+    'uganda_disability_seeing',
+    'uganda_disability_walking',
+    'uganda_dpt3_coverage',
+    'uganda_energy_cooking_charcoal',
+    'uganda_energy_cooking_electricity',
+    'uganda_energy_cooking_firewood',
+    'uganda_energy_cooking_gas',
+    'uganda_energy_cooking_paraffin_stove',
+    'uganda_energy_lighting_candle',
+    'uganda_energy_lighting_electricity',
+    'uganda_energy_lighting_firewood',
+    'uganda_energy_lighting_gas',
+    'uganda_energy_lighting_paraffin_lantern',
+    'uganda_energy_lighting_paraffin_tadooba',
+    'uganda_health_posts',
+    'uganda_hmis',
+    'uganda_household_number',
+    'uganda_household_san_cov',
+    'uganda_household_water_coverage',
+    'uganda_ipt2_coverage',
+    'uganda_leaving_exam_perf_rate',
+    'uganda_life_expectancy',
+    'uganda_overall_health',
+    'uganda_pop_dens',
+    'uganda_pop_growthrate',
+    'uganda_pop_sex_ratio',
+    'uganda_poverty_headcount',
+    'uganda_primary_enrol',
+    'uganda_primary_pupil_classroom_ratio',
+    'uganda_primary_pupil_classroom_ratio_gov',
+    'uganda_primary_pupil_classroom_ratio_priv',
+    'uganda_primary_pupil_stance_ratio',
+    'uganda_primary_pupil_stance_ratio_gov',
+    'uganda_primary_pupil_stance_ratio_priv',
+    'uganda_primary_sit_write',
+    'uganda_primary_sit_write_gov',
+    'uganda_primary_stu_teach_ratio',
+    'uganda_primary_stu_teach_ratio_gov',
+    'uganda_primary_stu_teach_ratio_priv',
+    'uganda_remittance',
+    'uganda_rural_safe_water',
+    'uganda_rural_water_func',
+    'uganda_secondary_enrol',
+    'uganda_secondary_sit_write',
+    'uganda_secondary_sit_write_gov',
+    'uganda_secondary_stu_classroom_ratio',
+    'uganda_secondary_stu_classroom_ratio_gov',
+    'uganda_secondary_stu_classroom_ratio_priv',
+    'uganda_secondary_stu_stance_ratio',
+    'uganda_secondary_stu_stance_ratio_gov',
+    'uganda_secondary_stu_stance_ratio_priv',
+    'uganda_secondary_stu_teach_ratio',
+    'uganda_secondary_stu_teach_ratio_gov',
+    'uganda_tb_success',
+    'uganda_total_pop',
+    'uganda_urban_pop',
+    'uganda_wash_perf_score',
+    'uganda_water_equity_rural',
+    'uganda_water_source_comm_func',
+    'uganda_water_women_position'
+  ],
+  uganda_indicator_percent: [
+    'uganda_agri_percent',
+    'uganda_donor_percent',
+    'uganda_educ_percent',
+    'uganda_health_percent',
+    'uganda_local_percent'
+  ],
+  uganda_indicator_ncu: [
+    'uganda_central_resources',
+    'uganda_donor_educ_spend',
+    'uganda_donor_resources',
+    'uganda_health_funding',
+    'uganda_igf_resources',
+    'uganda_primary_educ_funding'
+  ],
+  kenya_indicator_data: [
+    'kenya_births_attendance',
+    'kenya_births_attendance_5_years',
+    'kenya_births_notified',
+    'kenya_births_notified_5_years',
+    'kenya_child_mortality',
+    'kenya_children_breastfed',
+    'kenya_children_stunted',
+    'kenya_children_underweight',
+    'kenya_children_wasting',
+    'kenya_contraceptives',
+    'kenya_dependency_ratio',
+    'kenya_diarrhoea',
+    'kenya_disability',
+    'kenya_disability_cognition',
+    'kenya_disability_communication',
+    'kenya_disability_hearing',
+    'kenya_disability_mobility',
+    'kenya_disability_self_care',
+    'kenya_disability_visual',
+    'kenya_disability_wasting',
+    'kenya_secondary_ner_boys',
+    'kenya_albinism',
+    'kenya_avg_house_size',
+    'kenya_primary_av_size_gov',
+    'kenya_primary_av_size_priv',
+    'kenya_primary_ner_boys',
+    'kenya_primary_ner_girls',
+    'kenya_primary_stu_teach_ratio',
+    'kenya_primary_stu_teach_ratio_gov',
+    'kenya_primary_stu_teach_ratio_priv',
+    'kenya_safe_water',
+    'kenya_secondary_av_size_gov',
+    'kenya_secondary_av_size_priv',
+    'kenya_secondary_ner_girls',
+    'kenya_secondary_stu_teach_ratio',
+    'kenya_secondary_stu_teach_ratio_gov',
+    'kenya_secondary_stu_teach_ratio_priv',
+    'kenya_total_pop',
+    'kenya_urban_pop',
+    'kenya_ecde_ner_boys',
+    'kenya_ecde_ner_girls',
+    'kenya_energy_cooking_charcoal',
+    'kenya_energy_cooking_electricity',
+    'kenya_energy_cooking_firewood',
+    'kenya_energy_lighting_candle',
+    'kenya_energy_lighting_electricity',
+    'kenya_energy_lighting_gaslamp',
+    'enya_energy_lighting_paraffin',
+    'kenya_energy_lighting_solar',
+    'kenya_energy_lighting_wood',
+    'kenya_hdi',
+    'kenya_health_insurance_prop_pop',
+    'kenya_hhs_crop_production',
+    'kenya_hhs_irrigation',
+    'kenya_hhs_livestock_prod',
+    'kenya_hiv_prevalence',
+    'kenya_household_female_head',
+    'kenya_household_number',
+    'kenya_household_san_com',
+    'kenya_household_san_fac',
+    'kenya_household_water_com',
+    'kenya_malaria_drugs_under5',
+    'kenya_maternal_mortality',
+    'kenya_measles_immunisation',
+    'kenya_measles_vaccine',
+    'kenya_mosquito_nets',
+    'kenya_mosquito_nets_under5',
+    'kenya_per_cap_agric',
+    'kenya_per_cap_educ',
+    'kenya_per_cap_health',
+    'kenya_per_cap_wash',
+    'kenya_pop_birthrate',
+    'kenya_pop_deathrate',
+    'kenya_pop_dens',
+    'kenya_pop_disability_engage_econ',
+    'kenya_pop_sex_ratio',
+    'kenya_pop_working',
+    'kenya_pop_working_female',
+    'kenya_pop_working_male',
+    'kenya_poverty_food',
+    'kenya_poverty_gap',
+    'kenya_poverty_hardcore',
+    'kenya_poverty_headcount',
+    'kenya_poverty_meanpp_exp'
+  ],
+  kenya_indicator_percent: [
+    'kenya_educ_percent',
+    'kenya_health_percent',
+    'kenya_local_percent',
+    'kenya_conditional_percent',
+    'kenya_agri_percent',
+    'kenya_water_per_of_revenue',
+    'kenya_donor_percent',
+    'kenya_equitable_percent',
+    'kenya_gov_spend_pp'
+  ],
+  kenya_indicator_ncu: [
+    'kenya_donor_resources',
+    'kenya_health_funding',
+    'kenya_igf_resources',
+    'kenya_central_resources'
+  ]
 };
 
 const unchangedTables = [
@@ -148,7 +342,16 @@ export class DB {
   }
 
   fetchFromTable(options: FetchOptions): Promise<any> {
-    const { columnNames, indicator, entities, startYear = 0, endYear = 9999, limit = 1000000, offset = 0, filter } = options;
+    const {
+      columnNames,
+      indicator,
+      entities,
+      startYear = 0,
+      endYear = 9999,
+      limit = 1000000,
+      offset = 0,
+      filter
+    } = options;
     let entityName = '';
     let entityTable = 'di_entity';
     const mappedIndicator = getTableNameFromIndicator(indicator);
@@ -171,9 +374,17 @@ export class DB {
     }
 
     if (columnNames.indexOf('year') > -1) {
-      let where = filter && filter.length
-        ? PGPromise.as.format('WHERE year >= $1 AND year <= $2 $3:raw', [ startYear, endYear, this.formatFilters(filter) ])
-        : PGPromise.as.format('WHERE year >= $1 AND year <= $2', [ startYear, endYear ]);
+      let where =
+        filter && filter.length
+          ? PGPromise.as.format('WHERE year >= $1 AND year <= $2 $3:raw', [
+              startYear,
+              endYear,
+              this.formatFilters(filter)
+            ])
+          : PGPromise.as.format('WHERE year >= $1 AND year <= $2', [
+              startYear,
+              endYear
+            ]);
       if (mappedIndicator !== indicator) {
         where = where + PGPromise.as.format(' AND indicator_id = $1', [ indicator ]);
       }
@@ -229,9 +440,13 @@ export class DB {
       if (!this.validOperator(currentOptions[0].operator)) {
         throw new Error(`Invalid operator: ${currentOptions[0].operator}`);
       }
-      const AND = currentOptions[0].value !== 'NULL'
-        ? PGPromise.as.format(`${currentOptions[0].field} ${currentOptions[0].operator} $1`, [ currentOptions[0].value ])
-        : `${currentOptions[0].field} ${currentOptions[0].operator} ${currentOptions[0].value}`;
+      const AND =
+        currentOptions[0].value !== 'NULL'
+          ? PGPromise.as.format(
+              `${currentOptions[0].field} ${currentOptions[0].operator} $1`,
+              [ currentOptions[0].value ]
+            )
+          : `${currentOptions[0].field} ${currentOptions[0].operator} ${currentOptions[0].value}`;
 
       return index > 0 ? `${prevOuter} AND ${AND}` : `AND ${AND}`;
     }, '');
